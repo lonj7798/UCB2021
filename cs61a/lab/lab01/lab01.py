179c114692125b9e1ff82df40b45eda36aa678a3
Jewon <lonj7798@gmail.com>
2021-09-01 22:54:26 -0700

    hw

---
+++ cs61a/lab/lab01/lab01.py
@@ -0,0 +1,68 @@
+def falling(n, k):
+    """Compute the falling factorial of n to depth k.
+
+    >>> falling(6, 3)  # 6 * 5 * 4
+    120
+    >>> falling(4, 3)  # 4 * 3 * 2
+    24
+    >>> falling(4, 1)  # 4
+    4
+    >>> falling(4, 0)
+    1
+    """
+    "*** YOUR CODE HERE ***"
+    anw = 1
+    for num in range(n-k+1, n+1):
+        anw *= num
+        
+    return anw
+       
+
+def sum_digits(y):
+    """Sum all the digits of y.
+
+    >>> sum_digits(10) # 1 + 0 = 1
+    1
+    >>> sum_digits(4224) # 4 + 2 + 2 + 4 = 12
+    12
+    >>> sum_digits(1234567890)
+    45
+    >>> a = sum_digits(123) # make sure that you are using return rather than print
+    >>> a
+    6
+    """
+    "*** YOUR CODE HERE ***"
+    
+    digits = str(y)
+    anw = 0
+
+    for i in range(0, len(digits)):
+        anw += int(digits[i])
+        
+    return anw
+
+def double_eights(n):
+    """Return true if n has two eights in a row.
+    >>> double_eights(8)
+    False
+    >>> double_eights(88)
+    True
+    >>> double_eights(2882)
+    True
+    >>> double_eights(880088)
+    True
+    >>> double_eights(12345)
+    False
+    >>> double_eights(80808080)
+    False
+    """
+    "*** YOUR CODE HERE ***"
+    digits = str(n)
+    
+    while(digits[:-1]):
+        if (digits[-1] == '8') & (digits[-2] == '8'):
+                return True
+        
+        digits = digits[:-1]
+        
+    return False
